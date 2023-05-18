abb2afa00b87f4921041b756b87d06f7ae0f7b9c
anjupatil <anjupatil42@gmail.com>
2023-05-19 00:15:46 +0530

    submitting code-Fri May 19 2023 00:15:45 GMT+0530 (India Standard Time)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Modal = ({ show, onClose, children }) => {
+  const modalRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (show) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [show, onClose]);
+
   if (!show) {
-    return null; // Don't render the modal if show is false
+    return null;
   }
 
   return (
     <div className="modal-overlay">
-      <div className="modal-dialog">
+      <div ref={modalRef} className="modal-dialog">
         <button className="modal-close" onClick={onClose}>
           Close
         </button>
