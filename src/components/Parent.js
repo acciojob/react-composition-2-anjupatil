02ef4982ef34492abc3de83b20371953b1a266f3
anjupatil <anjupatil42@gmail.com>
2023-05-19 00:44:19 +0530

    submitting code-Fri May 19 2023 00:44:18 GMT+0530 (India Standard Time)

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -17,8 +17,8 @@ const Parent=()=>{
         <div className="parent">
             <h1>Parent Component</h1>
             <button onClick={handleButtonClick}>Show Modal</button>
-            <Modal show={showModal} onClose={handleCloseModal}>
-                <h2>Modal Content</h2>
+            <Modal className="modal" show={showModal} onClose={handleCloseModal}>
+                <h2 className="modal">Modal Content</h2>
                 <p className="modal">This is the content of the modal.</p>
             </Modal>
             
